8f5f15bff046c96b6e995e9571b3dd1e7d0e757c
alexmpereira <alexmpprog@gmail.com>
2020-01-06 12:56:27 -0300

    Configuração das views Home e Users

---
+++ Application/views/home/index.php
@@ -0,0 +1,7 @@
+<div class="container">
+  <div class="row">
+    <div class="col-8 offset-2 text-center" style="margin-top:150px">
+      <h1>Framework MVC com PHP</h1>
+    </div>
+  </div>
+</div>
